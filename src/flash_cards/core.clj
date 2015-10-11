698ab5c2aed553f484aca5ba3eb2be52356e3e9a
Nicholas Lydon <nickkell@gmail.com>
2015-10-11 23:49:22 +0100

    added phrases; made all-words function private and removed tests for it; phrase order is shuffled

--- src/flash_cards/core.clj
+++ src/flash_cards/core.clj
@@ -3,29 +3,36 @@
 
 (require 'flash-cards.word_map)
 
-(defn all-words [word-map]
+(defn ^:private all-words [word-map]
   (->>
     (concat word-map
-          (->>
-            word-map
-            (partition 2)
-            (map reverse)
-            flatten))
+            (->>
+              word-map
+              (partition 2)
+              (map reverse)
+              flatten))
     (partition 2)
     (map vec)))
 
 (defn show-words [word-map println read-line]
-  (loop [remaining (mapcat identity (repeat (all-words word-map)))]
-    (let [[question answer] (first remaining)]
-      (do
-        (println question)
-        (let [guess (read-line)]
-          (cond (not= guess "quit")
-                (do
-                  (if (= guess answer)
-                    (println "correct")
-                    (println (str "correct answer: " answer)))
-                  (recur (rest remaining)))))))))
+  (let [shuffle-phrases
+        (fn []
+          (->>
+            (all-words word-map)
+            (shuffle)))]
+    (loop [remaining (shuffle-phrases)]
+      (if (empty? remaining)
+          (recur (shuffle-phrases))
+          (let [[question answer] (first remaining)]
+            (do
+              (println question)
+              (let [guess (read-line)]
+                (cond (not= guess "quit")
+                      (do
+                        (if (= guess answer)
+                          (println "correct")
+                          (println (str "correct answer: " answer)))
+                        (recur (rest remaining)))))))))))
 
 (defn -main
   [& args]
--- test/flash_cards/core_test.clj
+++ test/flash_cards/core_test.clj
@@ -6,42 +6,52 @@
 (def english "How are you?")
 (def input [german english])
 
-(deftest all-phrases-contains-initial-phrases
-  (testing "should include base set"
-    (let [[question answer] (first (all-words input))]
-      (is (= question german))
-      (is (= answer english)))))
+(deftest continues-looping-over-words
+  (testing "should not stop when reaching the end of the phrase collection"
+    (let [printed-line (atom '())
+          answered-count (atom 0)]
+          (show-words input
+                      #(swap! printed-line (partial cons %1))
+                      #(if (= @answered-count 3)
+                            "quit"
+                            (do
+                              (swap! answered-count inc)
+                              "doesn't matter")))
+          (is (= 2
+                 (count (filter (partial = english) @printed-line))))
+          (is (= 2
+                 (count (filter (partial = german) @printed-line)))))))
 
-(deftest all-phrases-contains-initial-phrases-reversed
-  (testing "should include reverse of base set"
-    (let [[question answer] (second (all-words input))]
-      (is (= question english))
-      (is (= answer german)))))
 
 (deftest prints-correct-when-given-correct-answer
   (testing "should print correct when right"
     (let [printed-line (atom '())
-          read-line-count (atom -1)
-          user-input [english "quit"] ]
+          already-answered (atom false)]
       (show-words input
                   #(swap! printed-line (partial cons %1))
-                  #(do
-                      (swap! read-line-count inc)
-                      (user-input @read-line-count)))
+                  #(if @already-answered
+                       "quit"
+                       (do
+                         (swap! already-answered (constantly true))
+                         (if (= english (first @printed-line))
+                            german
+                            english))))
       (is (=
-            [english "correct" german]
-            (vec @printed-line))))))
+            "correct"
+            (second @printed-line))))))
 
 (deftest prints-real-answer-when-user-makes-mistake
   (testing "should print the actual answer when given incorrect guess"
     (let [printed-line (atom '())
-          read-line-count (atom -1)
-          user-input ["incorrect guess" "quit"] ]
+          already-answered (atom false)]
       (show-words input
                   #(swap! printed-line (partial cons %1))
-                  #(do
-                      (swap! read-line-count inc)
-                      (user-input @read-line-count)))
+                  #(if @already-answered
+                       "quit"
+                       (do
+                         (swap! already-answered (constantly true))
+                         "gobble-de-gook")))
       (is (=
-            [english (str "correct answer: " english) german]
-            (vec @printed-line))))))
+            (str "correct answer: "
+                 (if (= english (last @printed-line)) german english))
+            (second @printed-line))))))
