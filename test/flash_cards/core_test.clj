03d3c3799063748a8edf9dc7b4135e200378430b
Nicholas Lydon <nickkell@gmail.com>
2015-10-18 00:17:16 +0100

    changed to use a website instead of console

--- test/flash_cards/core_test.clj
+++ test/flash_cards/core_test.clj
@@ -5,53 +5,25 @@
 (def german "Wie geht's?")
 (def english "How are you?")
 (def input [german english])
+(def request-handler (make-handler (constantly [input])))
+(defn long-string [& strings] (clojure.string/join "" strings))
 
-(deftest continues-looping-over-words
-  (testing "should not stop when reaching the end of the phrase collection"
-    (let [printed-line (atom '())
-          answered-count (atom 0)]
-          (show-words input
-                      #(swap! printed-line (partial cons %1))
-                      #(if (= @answered-count 3)
-                            "quit"
-                            (do
-                              (swap! answered-count inc)
-                              "doesn't matter")))
-          (is (= 2
-                 (count (filter (partial = english) @printed-line))))
-          (is (= 2
-                 (count (filter (partial = german) @printed-line)))))))
-
-
-(deftest prints-correct-when-given-correct-answer
-  (testing "should print correct when right"
-    (let [printed-line (atom '())
-          already-answered (atom false)]
-      (show-words input
-                  #(swap! printed-line (partial cons %1))
-                  #(if @already-answered
-                       "quit"
-                       (do
-                         (swap! already-answered (constantly true))
-                         (if (= english (first @printed-line))
-                            german
-                            english))))
-      (is (=
-            "correct"
-            (second @printed-line))))))
-
-(deftest prints-real-answer-when-user-makes-mistake
-  (testing "should print the actual answer when given incorrect guess"
-    (let [printed-line (atom '())
-          already-answered (atom false)]
-      (show-words input
-                  #(swap! printed-line (partial cons %1))
-                  #(if @already-answered
-                       "quit"
-                       (do
-                         (swap! already-answered (constantly true))
-                         "gobble-de-gook")))
-      (is (=
-            (str "correct answer: "
-                 (if (= english (last @printed-line)) german english))
-            (second @printed-line))))))
+(deftest first-request-presents-first-phrase
+  (testing "should show form with phrase to be translated and input element to write answer"
+    (let [response (request-handler {:uri ""})]
+      (is (= (:status response) 200))
+      (is (= (:body response)
+             (long-string
+               "<form action=\"make-guess\">"
+                "<div>"
+                  "<div>"
+                    german
+                  "</div>"
+                  "<div>"
+                    "<input autocomplete=\"off\" autocorrect=\"off\" name=\"guess\" type=\"text\" />"
+                    "<div>"
+                      "<a href=\"/\">Start again</a>"
+                    "</div>"
+                  "</div>"
+                  "</div>"
+                "</form>"))))))
