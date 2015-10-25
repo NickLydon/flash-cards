72a5b9074d21e621c876969e654e8f4efa7160ff
Nicholas Lydon <nickkell@gmail.com>
2015-10-25 22:10:46 +0000

    fixed test

--- test/flash_cards/core_test.clj
+++ test/flash_cards/core_test.clj
@@ -10,20 +10,37 @@
 
 (deftest first-request-presents-first-phrase
   (testing "should show form with phrase to be translated and input element to write answer"
-    (let [response (request-handler {:uri ""})]
+    (let [response (request-handler {:uri ""})
+          expected (long-string
+             "<!DOCTYPE html>\n"
+             "<html>"
+               "<head></head>"
+               "<body>"
+                "<form action=\"make-guess\">"
+                 "<div>"
+                   "<div>"
+                     german
+                   "</div>"
+                   "<div>"
+                     "<input autocomplete=\"off\" autocorrect=\"off\" name=\"guess\" type=\"text\">"
+                     "<div>"
+                       "<a href=\"/\">Start again</a>"
+                     "</div>"
+                   "</div>"
+                   "</div>"
+                 "</form>"
+               "</body>"
+             "</html>")]
       (is (= (:status response) 200))
-      (is (= (:body response)
-             (long-string
-               "<form action=\"make-guess\">"
-                "<div>"
-                  "<div>"
-                    german
-                  "</div>"
-                  "<div>"
-                    "<input autocomplete=\"off\" autocorrect=\"off\" name=\"guess\" type=\"text\" />"
-                    "<div>"
-                      "<a href=\"/\">Start again</a>"
-                    "</div>"
-                  "</div>"
-                  "</div>"
-                "</form>"))))))
+      (let [first-diff
+            (loop [i 0
+                   actual   (seq (:body response))
+                   expected (seq expected)]
+                    (cond (not-every? empty? [actual expected])
+                      (let [comp1 (first actual)
+                            comp2 (first expected)]
+                            (if (= comp1 comp2)
+                              (recur (inc i) (rest actual) (rest expected))
+                              [i comp1 comp2]))))]
+
+              (is (= expected (:body response)))))))    
