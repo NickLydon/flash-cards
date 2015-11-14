8ad9d5437215654605e0e629054c2363fe09a44f
Nicholas Lydon <nickkell@gmail.com>
2015-11-14 19:00:56 +0000

    added ability to filter phrases by group/tag

--- src/flash_cards/core.clj
+++ src/flash_cards/core.clj
@@ -18,7 +18,7 @@
       :headers {"Content-Type" "text/html;charset=utf-8"}
       :body body})
 
-(defn make-body [{id :id phrase :phrase}]
+(defn make-body [{id :id phrase :phrase} tag]
   (let [language (if (zero? (rand-int 2)) :german :english)
         index (rand-index (language phrase))
         phrase-to-translate (nth (language phrase) index)]
@@ -29,43 +29,59 @@
           [:input {:type "text" :name "guess" :autocomplete "off" :autocorrect "off"}]
           [:input {:type "hidden" :name "id" :value id}]
           [:input {:type "hidden" :name "language" :value (str language)}]
-         [:div [:a {:href "/"} "Start again"]]]]]))
+          (when tag [:input {:type "hidden" :name "tag" :value tag}])
+         [:div [:a {:href "/"} "Start again"]]
+         [:ul (for [tag @all-tags] [:li [:a {:href (str "/?tag=" tag)} tag]])]]]]))
 
-(defn get-new-phrase [current-word-map]
+(defn get-tagged-phrases [current-word-map tag]
+  (reduce-kv (fn [acc k v]
+               (if (some #{tag} (:tags v))
+                   (assoc acc k v)
+                   acc))
+             {}
+             current-word-map))
+
+(defn get-new-phrase
+ ([current-word-map]
   (let [ks (keys current-word-map)
         key (nth ks (rand-index current-word-map))]
-    {:id key :phrase (current-word-map key)}))
+      {:id key :phrase (current-word-map key)}))
+ ([current-word-map tag]
+  (if tag
+      (get-new-phrase (get-tagged-phrases current-word-map tag))
+      (get-new-phrase current-word-map))))
 
-(defn restart-guessing [current-word-map]
-  (do (reset! score 0)
-    (make-response-200
-      (html5
-        [:head]
-        [:body (make-body (get-new-phrase current-word-map))]))))
+(defn restart-guessing
+  ([current-word-map tag]
+   (do (reset! score 0)
+      (make-response-200
+        (html5
+          [:head]
+          [:body (make-body (get-new-phrase current-word-map tag)
+                            tag)])))))
 
 (defmacro log-sym [sym] `(println ~(str (second `(name ~sym))) ~sym))
 
 (defn make-handler [current-word-map]
-    (fn [request]
-      (if (.contains (request :uri) "make-guess")
-          (let [query-string (keywordize-keys (form-decode (:query-string request)))
-                guess (:guess query-string)
+  (fn [request]
+    (let [query-string (keywordize-keys (form-decode (or (:query-string request) "")))
+          tag (:tag query-string)]
+      (if (not (.contains (request :uri) "make-guess"))
+          (restart-guessing current-word-map tag)
+
+          (let [guess (:guess query-string)
                 id (:id query-string)
                 language (if (= :german (read-string (:language query-string))) :english :german)
                 matching-phrase (get current-word-map id)
                 translations (language matching-phrase)
-                phrase-to-translate (get-new-phrase current-word-map)
+                phrase-to-translate (get-new-phrase current-word-map tag)
                 marks    (map (partial jaro-winkler guess) translations)
                 correct? (some (partial = 1.0) marks)
                 close?   (some (partial < 0.9) marks)]
             (do
-              (swap! score
-                     (cond correct?
-                            inc
-                           close?
-                            identity
-                           :else
-                            dec))
+              (swap! score (cond correct? inc
+                                 close? identity
+                                 :else dec))
               (make-response-200
                   (html5
                     [:head]
@@ -73,9 +89,7 @@
                       [:div
                         [:div (str "Score: " @score)]
                         [:div (if correct? "Correct!" (str "Correct answers include: " (clojure.string/join ", " translations)))
-                            (make-body phrase-to-translate)]]]))))
-
-          (restart-guessing current-word-map))))
+                            (make-body phrase-to-translate tag)]]]))))))))
 
 (defn to-dict [phrases]
   (reduce (fn [acc next] (assoc acc (str (java.util.UUID/randomUUID)) next))
--- test/flash_cards/core_test.clj
+++ test/flash_cards/core_test.clj
@@ -8,31 +8,42 @@
 (def english "How are you?")
 (def input {:german [german] :english [english]})
 (def request-handler (make-handler {1 input}))
-(defn long-string [& strings] (clojure.string/join "" strings))
+(defn long-string [fst & strings]
+  (if fst
+      (if (coll? fst)
+          (str (apply long-string fst) (long-string strings))
+          (str fst (long-string strings)))
+      ""))
+(def all-tags ["countries" "languages" "nationalities" "partings" "greetings"])
 
 (fact "should show form with phrase to be translated and input element to write answer"
     (let [response (request-handler {:uri ""})
+          lang (if (.contains (:body response) (str :german)) :german :english)
+          lang-map {:german german :english english}
           expected (long-string
                     "<!DOCTYPE html>\n"
                     "<html>"
-                    "<head></head>"
-                    "<body>"
-                    "<form action=\"make-guess\">"
-                    "<div>"
-                    "<div>"
-                     german
-                    "</div>"
-                    "<div>"
-                     "<input autocomplete=\"off\" autocorrect=\"off\" name=\"guess\" type=\"text\">"
-                     "<input name=\"id\" type=\"hidden\" value=\"1\">"
-                     "<input name=\"language\" type=\"hidden\" value=\"" :german "\">"                  
-                     "<div>"
-                       "<a href=\"/\">Start again</a>"
-                     "</div>"
-                    "</div>"
-                    "</div>"
-                    "</form>"
-                    "</body>"
+                      "<head></head>"
+                      "<body>"
+                        "<form action=\"make-guess\">"
+                          "<div>"
+                            "<div>"
+                             (lang-map lang)
+                            "</div>"
+                            "<div>"
+                               "<input autocomplete=\"off\" autocorrect=\"off\" name=\"guess\" type=\"text\">"
+                               "<input name=\"id\" type=\"hidden\" value=\"1\">"
+                               "<input name=\"language\" type=\"hidden\" value=\"" lang "\">"
+                               "<div>"
+                                 "<a href=\"/\">Start again</a>"
+                               "</div>"
+                               "<ul>"
+                                  (for [tag all-tags] (str "<li>" "<a href=\"/?tag=" tag "\">" tag "</a></li>"))
+                               "</ul>"
+                            "</div>"
+                          "</div>"
+                        "</form>"
+                      "</body>"
                     "</html>")]
       (:status response) => 200
       (:body response) => expected))
